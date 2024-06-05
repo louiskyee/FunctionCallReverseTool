b60deea8ad3c63db8821b22b6e15e606473a0465
louiskyee <bolin8017@gmail.com>
2024-06-05 16:46:33 +0900

    Add function call script

---
+++ Ghidra/get_function_call.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Input parameters
+ghidra_headless_path=$1
+program_folder=$2
+
+# Get the directory of the currently executing script
+current_dir=$(pwd)
+python_script_path="${current_dir}/ghidra_function_script.py"
+project_name="$(basename "${program_folder}")"
+output_dir="${current_dir}/${project_name}_disassemble"
+
+# Set directory path variables based on input parameters
+project_folder="${output_dir}/ghidra_projects"
+result_folder="${output_dir}/results"
+split_dir="${output_dir}/split_folders"
+
+# Set the maximum number of CPUs to use
+max_cpu=$(nproc)
+
+if [ -d "${output_dir}" ]; then
+    rm -rf "${output_dir}"
+fi
+mkdir -p "${output_dir}"
+
+# Create the project_folder, result_folder, and split_dir
+mkdir -p "${project_folder}"
+mkdir -p "${result_folder}"
+mkdir -p "${split_dir}"
+
+# Make sure time.txt file does not exist before execution to avoid retaining old results
+time_file_name="${output_dir}/${project_name}_disassemble_time.txt"
+rm -f "${time_file_name}"
+
+# Split the input program_folder into max_cpu subfolders
+file_count=$(find "${program_folder}" -type f | wc -l)
+if [ $file_count -lt $max_cpu ]; then
+    max_cpu=$file_count
+fi
+files_per_folder=$((file_count / max_cpu))
+remainder=$((file_count % max_cpu))
+
+index=1
+folder_index=1
+for file in "${program_folder}"/*; do
+    if [ $((index % (files_per_folder + 1))) -eq 0 ] && [ $folder_index -lt $max_cpu ]; then
+        folder_index=$((folder_index + 1))
+    fi
+    subfolder="${split_dir}/${project_name}_${folder_index}"
+    mkdir -p "${subfolder}"
+    cp "${file}" "${subfolder}"
+    index=$((index + 1))
+done
+
+# Handle the remaining files
+if [ $remainder -gt 0 ]; then
+    subfolder="${split_dir}/${project_name}_${max_cpu}"
+    mkdir -p "${subfolder}"
+    for file in $(find "${program_folder}" -type f | tail -n $remainder); do
+        cp "${file}" "${subfolder}"
+    done
+fi
+
+# Create corresponding subfolders in project_folder for each split_folder
+for subfolder in "${split_dir}"/*; do
+    subfolder_name=$(basename "${subfolder}")
+    corresponding_project_folder="${project_folder}/${subfolder_name}"
+    mkdir -p "${corresponding_project_folder}"
+done
+
+start_time=$(date +%s)
+
+# Process each subfolder in parallel using GNU Parallel
+find "${split_dir}" -mindepth 1 -maxdepth 1 -type d | parallel --jobs "${max_cpu}" "${ghidra_headless_path}" "${project_folder}/{/}" "{/}" -import "{}" -scriptPath "$(dirname "${python_script_path}")" -postScript "$(basename "${python_script_path}")" "${output_dir}" "${result_folder}" -max-cpu $(nproc)
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))  # Calculate the total execution time in seconds
+# Append the total execution time to the time.txt file
+echo "Total Execution Time: ${execution_time} seconds" >> "${time_file_name}"
---
+++ Ghidra/ghidra_function_script.py
@@ -0,0 +1,99 @@
+import os
+import time
+import json
+import logging
+from ghidra.app.util.headless import HeadlessScript
+from ghidra.program.model.symbol import Reference
+
+# Get script arguments and determine the save folder
+argv = getScriptArgs()
+
+try:
+    # Set save folder
+    if len(argv) == 2:
+        output_folder = argv[0]
+        results_folder = argv[1]
+    elif len(argv) == 1:
+        output_folder = argv[0]
+        results_folder = os.path.join(output_folder, 'results')
+    elif len(argv) == 0:
+        output_folder = os.getcwd()
+        results_folder = os.path.join(os.getcwd(), 'results')
+    else:
+        raise ValueError("Invalid number of arguments")
+except Exception as e:
+    error_message = "An error occurred while setting parameters: {}".format(e)
+    logging.error(error_message, exc_info=True)
+
+program_name = currentProgram.getName()
+program_folder = os.path.join(results_folder, program_name)
+
+# Create the program-specific directory
+if not os.path.exists(program_folder):
+    os.makedirs(program_folder)
+
+# Set up logging
+log_file_path = os.path.join(output_folder, 'extract_function.log')
+logging.basicConfig(filename=log_file_path, level=logging.ERROR,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+# Determine file paths for DOT file and JSON file
+dot_file_path = os.path.join(program_folder, program_name + '.dot')
+json_file_path = os.path.join(program_folder, program_name + '.json')
+
+try:
+    start_time = time.time()
+    fm = currentProgram.getFunctionManager()
+    funcs = fm.getFunctions(True)
+
+    dot_lines = ["digraph FunctionXrefs {"]
+    functions_info = {}
+
+    # Collecting all lines to write in batch
+    for func in funcs:
+        entry_point = func.getEntryPoint()
+        entry_point_offset = hex(entry_point.getOffset())
+        name = func.getName()
+
+        # Prepare function information for JSON
+        functions_info[name] = {
+            "function_address": entry_point_offset,
+            "instructions": []
+        }
+
+        dot_lines.append('  "{}" [label="{}"];'.format(entry_point_offset, name))
+
+        # Extracting instructions for each function
+        for instruction in currentProgram.getListing().getInstructions(func.getBody(), True):
+            instruction_info = {
+                "instruction_address": str(instruction.getAddress()),
+                "instruction": str(instruction)
+            }
+            functions_info[name]["instructions"].append(instruction_info)
+
+        callees = func.getCalledFunctions(None)
+        for callee in callees:
+            # get the entry point of the callee function
+            callee_entry_point = callee.getEntryPoint()
+            callee_entry_point_offset = hex(callee_entry_point.getOffset())
+            # Write DOT file content
+            dot_lines.append('  "{}" -> "{}";'.format(entry_point_offset, callee_entry_point_offset))
+
+    dot_lines.append("}")
+
+    # Writing to DOT file
+    with open(dot_file_path, "w") as dot_file:
+        dot_file.write("\n".join(dot_lines))
+
+    # Writing to JSON file
+    with open(json_file_path, "w") as json_file:
+        json.dump(functions_info, json_file, indent=4)
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    with open(os.path.join(output_folder, 'execution_times.log'), 'a', newline='', encoding='utf-8') as f:
+        f.write("{},{:.2f}\n".format(program_name, execution_time))
+
+except Exception as e:
+    error_message = "An error occurred while writing the files: {}".format(e)
+    logging.error(error_message, exc_info=True)
